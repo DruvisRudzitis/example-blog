df18b03dc84a765943ed5e7f4b5c9034cbc4fc9e
Druvis Rudzītis <69005245+DruvisRudzitis@users.noreply.github.com>
2020-11-21 15:33:50 +0200

    Add files via upload

---
+++ tests/Feature/ArticlesControllerTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ArticlesControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testIndex(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $article = Article::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->get(route('articles.index'));
+        $response->assertSee($article->title);
+        $response->assertSee($article->content);
+    }
+
+    public function testStore(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $article = Article::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->get(route('articles.create'));
+        $response->assertSee($article->title);
+        $response->assertSee($article->content);
+
+    }
+
+    public function testShow(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $article = Article::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->get(route('articles.show'));
+        $response->assertSee($article->title);
+        $response->assertSee($article->content);
+    }
+
+    public function testEdit(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $article = Article::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->get(route('articles.edit'));
+        $response->assertSee($article->title);
+        $response->assertSee($article->content);
+    }
+
+
+    public function testUpdate(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $article = Article::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->followingRedirects();
+
+        $response = $this->put(route('articles.update'), [
+            'title' => 'Example title',
+            'content' => 'Example content'
+        ]);
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('articles', [
+            'title' => 'Example title',
+            'content' => 'Example content'
+        ]);
+    }
+
+
+
+    public function testCreateNewArticle(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $this->followingRedirects();
+
+        $response = $this->post(route('articles.store'), [
+            'title' => 'Example title',
+            'content' => 'Example content'
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('articles', [
+            'user_id' => $user->id,
+            'title' => 'Example title',
+            'content' => 'Example content'
+        ]);
+    }
+
+    public function testDeleteArticle(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $article = Article::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $this->assertDatabaseHas('articles', [
+            'user_id' => $user->id,
+            'title' => $article->title,
+            'content' => $article->content
+        ]);
+
+        $this->followingRedirects();
+
+        $response = $this->delete(route('articles.destroy', $article));
+
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('articles', [
+            'user_id' => $user->id,
+            'title' => $article->title,
+            'content' => $article->content
+        ]);
+    }
+}
